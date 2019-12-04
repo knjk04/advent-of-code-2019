a381f695f005cae574b5d3b36bb3ec716615a816
Karan Kumar <karan.n.j.kumar@gmail.com>
2019-12-04 20:09:29 +0000

    day 3, part 2. WIP

--- src/main/java/solutions/Day3_Solution.java
+++ src/main/java/solutions/Day3_Solution.java
@@ -8,8 +8,7 @@ import java.util.stream.IntStream;
 
 public class Day3_Solution {
 
-    private final int originX = 0;
-    private final int originY = 0;
+//    private int tempSteps = 0; // I don't think this is a good idea
 
     public static void main(String[] args) {
         List<String> input = Util.readInput("/day03.txt");
@@ -28,45 +27,64 @@ public class Day3_Solution {
             }
         }
 
-        System.out.println("All paths in set 1");
         ArrayList<Coordinates> allPointsPath1 = calculateAllPoints(directions1);
-        System.out.println("All paths in set 2");
         ArrayList<Coordinates> allPointsPath2 = calculateAllPoints(directions2);
         ArrayList<Coordinates> intersectionPoints = getIntersectionPoints(allPointsPath1, allPointsPath2);
+//        shortestIntersectionManhattan(intersectionPoints);
 
-        shortestIntersectionManhattan(intersectionPoints);
+
+        runPartTwo(intersectionPoints);
+    }
+
+    private void runPartTwo(ArrayList<Coordinates> intersectionPoints) {
+        int minSteps = Integer.MAX_VALUE;
+
+        for (Coordinates c : intersectionPoints) {
+//            if (c.steps < minSteps) {
+//                minSteps = c.steps;
+//            }
+            minSteps = Math.min(minSteps, c.steps);
+        }
+
+        // 9394: too high
+        System.out.println("Min steps = " + minSteps);
+
+    }
+
+    private boolean noMatch(HashSet<Coordinates> hCoords, Coordinates c1) {
+        return hCoords.stream().noneMatch(c -> (c.x == c1.x && c.y == c1.y));
     }
 
     private void shortestIntersectionManhattan(ArrayList<Coordinates> intersectionPoints) {
         int minManhattan = Integer.MAX_VALUE; // just a default
         for (Coordinates c : intersectionPoints) {
             // Manhattan: |x1 - x2| + |y1 - y2|
+            int originX = 0;
+            int originY = 0;
             int manhattan = Math.abs(originX - c.x) + Math.abs(originY - c.y);
-            System.out.println("manhattan = " + manhattan);
             if (manhattan < minManhattan) {
                 minManhattan = manhattan;
-                System.out.println("shortestIntersectionManhattan x = " + c.x);
-                System.out.println("shortestIntersectionManhattan y = " + c.y);
             }
+//            minManhattan = Math.min(manhattan, minManhattan);
         }
-        System.out.println("intersectionPoints size = " + intersectionPoints.size());
-        // 1525: too high
-        // 1167: too high
-        // 1160: too high
         System.out.println("Minimum manhattan = " + minManhattan);
     }
 
     private ArrayList<Coordinates> getIntersectionPoints(ArrayList<Coordinates> allPointsPath1,
                                                          ArrayList<Coordinates> allPointsPath2) {
         ArrayList<Coordinates> intersectionPoints = new ArrayList<>();
+
+        System.out.println("\nIntersection points:");
         for (Coordinates c1 : allPointsPath1) {
             for (Coordinates c2 : allPointsPath2) {
                 if (c1.x == c2.x && c1.y == c2.y) {
-                    intersectionPoints.add(c1);
-                    System.out.println("c1 intersection x = " + c1.x);
-                    System.out.println("c1 intersection y = " + c1.y);
-                    System.out.println("c2 intersection x = " + c2.x);
-                    System.out.println("c2 intersection y = " + c2.y);
+                    Coordinates coordinates = new Coordinates();
+                    coordinates.x = c1.x;
+                    coordinates.y = c1.y;
+                    coordinates.steps = c1.steps + c2.steps;
+                    System.out.println("\nIntersection at (" + c1.x + ", " + c1.y + "). c1 steps = " +
+                            c1.steps + ". c2.steps = " + c2.steps + "\n");
+                    intersectionPoints.add(coordinates);
                     break;
                 }
             }
@@ -78,31 +96,76 @@ public class Day3_Solution {
         directions1.removeIf(Predicate.isEqual(""));
         int currentX = 0, currentY = 0;
         ArrayList<Coordinates> allPointsPath = new ArrayList<>();
+        HashSet<Coordinates> visitedCoordinates = new HashSet<>();
+
+        int steps = 0;
+        System.out.println("\ncalculateAllPoints()");
         for (String s : directions1) {
             char moveDirection = s.charAt(0);
             int moveBy = Integer.parseInt(s.substring(1));
 
+            System.out.println("\nmove = " + s);
+            System.out.println("steps after move = " + (steps + moveBy));
+
+//            tempSteps = steps;
+
+            int finalSteps = steps;
             int finalCurrentX = currentX;
             int finalCurrentY = currentY;
             if (moveDirection == 'L') {
-//                IntStream.rangeClosed(currentX, (currentX - moveBy)).forEach(n -> {
-                    IntStream.rangeClosed((currentX - moveBy), currentX).forEach(n -> {
+                IntStream.rangeClosed((currentX - moveBy), currentX).forEach(n -> {
                     Coordinates coordinates = new Coordinates();
                     coordinates.x = n;
                     coordinates.y = finalCurrentY;
-                    if (!(coordinates.x == 0 && coordinates.y == 0)) {
+//                    coordinates.steps = finalSteps;
+                    coordinates.steps = finalSteps + Math.abs(n);
+                    if ((!(coordinates.x == 0 && coordinates.y == 0)) && (noMatch(visitedCoordinates, coordinates))) {
                         allPointsPath.add(coordinates);
+                        visitedCoordinates.add(coordinates);
+                    } else if (!(coordinates.x == 0 && coordinates.y == 0)) {
+//                        coordinates.steps = visitedCoordinates.
+//                        System.out.println("match 1");
+                        for (Coordinates c : visitedCoordinates) {
+                            System.out.println("L match entered");
+                            if (c.x == coordinates.x && c.y == coordinates.y) {
+                                System.out.println("L match");
+                                coordinates.steps = c.steps; // overwrite
+                                allPointsPath.add(coordinates);
+                                break;
+                            }
+                        }
                     }
                 });
+//                allPointsPath.add(coordinates);
                 currentX -= moveBy;
             } else if (moveDirection == 'R') {
                 IntStream.rangeClosed(currentX, (currentX + moveBy)).forEach(n -> {
                     Coordinates coordinates = new Coordinates();
                     coordinates.x = n;
                     coordinates.y = finalCurrentY;
-                    if (!(coordinates.x == 0 && coordinates.y == 0)) {
+//                    coordinates.steps = finalSteps;
+                    coordinates.steps = finalSteps + Math.abs(n);
+
+//                    System.out.println("\n" + s + " x = " + coordinates.x);
+//                    System.out.println(s + " y = " + coordinates.y);
+//                    System.out.println(s + " steps = " + coordinates.steps);
+
+                    if ((!(coordinates.x == 0 && coordinates.y == 0)) && (noMatch(visitedCoordinates, coordinates))) {
+                        visitedCoordinates.add(coordinates);
                         allPointsPath.add(coordinates);
+                    } else if (!(coordinates.x == 0 && coordinates.y == 0)) {
+//                        coordinates.steps = visitedCoordinates.
+                        for (Coordinates c : visitedCoordinates) {
+                            System.out.println("R match entered");
+                            if (c.x == coordinates.x && c.y == coordinates.y) {
+                                System.out.println("match");
+                                coordinates.steps = c.steps; // overwrite
+                                allPointsPath.add(coordinates);
+                                break;
+                            }
+                        }
                     }
+//                    allPointsPath.add(coordinates);
                 });
                 currentX += moveBy;
             } else if (moveDirection == 'U') {
@@ -110,39 +173,76 @@ public class Day3_Solution {
                     Coordinates coordinates = new Coordinates();
                     coordinates.x = finalCurrentX;
                     coordinates.y = n;
-                    if (!(coordinates.x == 0 && coordinates.y == 0)) {
+//                    coordinates.steps = finalSteps;
+                    coordinates.steps = finalSteps + Math.abs(n);
+//                    System.out.println("\n" + s + " x = " + coordinates.x);
+//                    System.out.println(s + " y = " + coordinates.y);
+//                    System.out.println(s + " steps = " + coordinates.steps);
+                    if ((!(coordinates.x == 0 && coordinates.y == 0)) && (noMatch(visitedCoordinates, coordinates))) {
                         allPointsPath.add(coordinates);
+                        visitedCoordinates.add(coordinates);
+                    } else if (!(coordinates.x == 0 && coordinates.y == 0)) {
+//                        coordinates.steps = visitedCoordinates.
+                        for (Coordinates c : visitedCoordinates) {
+                            System.out.println("U match entered");
+                            if (c.x == coordinates.x && c.y == coordinates.y) {
+                                System.out.println("match");
+                                coordinates.steps = c.steps; // overwrite
+                                allPointsPath.add(coordinates);
+                                break;
+                            }
+                        }
                     }
+//                    allPointsPath.add(coordinates);
                 });
                 currentY += moveBy;
             } else if (moveDirection == 'D') {
-//                System.out.println("D. currentX = " + currentX);
-//                System.out.println("D. currentY = " + currentY);
-//                System.out.println("D. moveBy = " + moveBy);
-//                IntStream.rangeClosed(currentY, (currentY - moveBy)).forEach(n -> {
                 IntStream.rangeClosed((currentY - moveBy), currentY).forEach(n -> {
                     Coordinates coordinates = new Coordinates();
                     coordinates.x = finalCurrentX;
                     coordinates.y = n;
-//                    System.out.println("D x: " + coordinates.x);
-//                    System.out.println("D y: " + coordinates.y);
-                    if (!(coordinates.x == 0 && coordinates.y == 0)) {
+//                    coordinates.steps = finalSteps;
+                    coordinates.steps = finalSteps + Math.abs(n);
+//                    System.out.println("\n" + s + " x = " + coordinates.x);
+//                    System.out.println(s + " y = " + coordinates.y);
+//                    System.out.println(s + " steps = " + coordinates.steps);
+                    if ((!(coordinates.x == 0 && coordinates.y == 0)) && (noMatch(visitedCoordinates, coordinates))) {
+                        visitedCoordinates.add(coordinates);
                         allPointsPath.add(coordinates);
+                    } else if (!(coordinates.x == 0 && coordinates.y == 0)) {
+//                        coordinates.steps = visitedCoordinates.
+                        for (Coordinates c : visitedCoordinates) {
+                            System.out.println("D match entered");
+                            if (c.x == coordinates.x && c.y == coordinates.y) {
+                                System.out.println("match");
+                                coordinates.steps = c.steps; // overwrite
+                                allPointsPath.add(coordinates);
+                                break;
+                            }
+                        }
                     }
+//                    allPointsPath.add(coordinates);
                 });
                 currentY -= moveBy;
             }
+            steps += Math.abs(moveBy);
+            System.out.println("\n" + s + ": updated steps = " + steps);
         }
 
-        for (Coordinates c : allPointsPath) {
-            System.out.println("allPoints x: " + c.x);
-            System.out.println("allPoints y: " + c.y);
+        for (Coordinates c : visitedCoordinates) {
+            System.out.println("\nvisited:");
+            System.out.println("x = " + c.x);
+            System.out.println("visited:");
+            System.out.println("y = " + c.y);
+            System.out.println("visited. steps = " + c.steps);
         }
 
         return allPointsPath;
     }
 
+
     private static class Coordinates {
-        private int x = 0, y = 0;
+        int x = 0, y = 0;
+        private int steps = 0; // for part 2
     }
 }
